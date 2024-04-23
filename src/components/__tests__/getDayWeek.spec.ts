61a88597e1cc1804de1ff0873ba14b84cea77655
Ejex Xeffer <ejexefer@gmail.com>
2024-04-24 03:41:17 +0500

    fix: get other branch behavior is cover by test

--- src/components/__tests__/getDayWeek.spec.ts
+++ src/components/__tests__/getDayWeek.spec.ts
@@ -11,4 +11,7 @@ describe('getDayWeek', () => {
   it('get 20 day on month by day of week', async () => {
     expect(getDayWeek(2024, 1, 20, 0)).toEqual(2)
   })
+  it('get 26 day on month by day of week, that is equal 6', async () => {
+    expect(getDayWeek(2024, 1, 25, 1)).toEqual(6)
+  })
 })
