9267d1f85873270eab4450570cc382cc49be9fac
Ejex Xeffer <ejexefer@gmail.com>
2024-05-02 00:07:49 +0500

    refactor: delete comments

--- src/components/DatePicker/utils/saveDayChose.ts
+++ src/components/DatePicker/utils/saveDayChose.ts
@@ -4,11 +4,8 @@ export const saveDayChose = (arrays: TCalendarArr, day: number) => {
   let rightDay: IDay | undefined = undefined
   let i = 0
   while (!rightDay && i < arrays.length) {
-    console.log('!!!!!!!!!!!!!!!!!!')
     rightDay = arrays[i].find(({ id, value }) => {
-      console.log('id', id)
       if (id < 8 && Number(value) < 8 && value !== null) {
-        console.log('day', value, day)
         return value === day
       }
       if (id > 7 && id < 29 && value !== null) {
@@ -23,6 +20,5 @@ export const saveDayChose = (arrays: TCalendarArr, day: number) => {
     })
     i++
   }
-  console.log('rightDay', rightDay)
   return rightDay
 }
